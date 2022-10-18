2b7ffad7ac350608fe58939d21d471936e67d398
Pig Fang <g-plane@hotmail.com>
2022-10-18 10:54:15 +0800

    add attributes transformation

--- src/lib.rs
+++ src/lib.rs
@@ -23,8 +23,52 @@ impl VueJsxTransformVisitor {
         }
     }
 
-    fn transform_props(&mut self) -> ObjectLit {
-        todo!()
+    fn transform_attrs(&mut self, attrs: &[JSXAttrOrSpread]) -> ObjectLit {
+        ObjectLit {
+            span: DUMMY_SP,
+            props: attrs
+                .iter()
+                .map(|jsx_attr_or_spread| match jsx_attr_or_spread {
+                    JSXAttrOrSpread::JSXAttr(jsx_attr) => {
+                        PropOrSpread::Prop(Box::new(Prop::KeyValue(KeyValueProp {
+                            key: match &jsx_attr.name {
+                                JSXAttrName::Ident(ident) => PropName::Str(quote_str!(&ident.sym)),
+                                JSXAttrName::JSXNamespacedName(name) => PropName::Str(quote_str!(
+                                    format!("{}:{}", name.ns.sym, name.name.sym)
+                                )),
+                            },
+                            value: jsx_attr
+                                .value
+                                .as_ref()
+                                .map(|value| match value {
+                                    JSXAttrValue::Lit(lit) => Box::new(Expr::Lit(lit.clone())),
+                                    JSXAttrValue::JSXExprContainer(JSXExprContainer {
+                                        expr: JSXExpr::Expr(expr),
+                                        ..
+                                    }) => expr.clone(),
+                                    JSXAttrValue::JSXExprContainer(JSXExprContainer {
+                                        expr: JSXExpr::JSXEmptyExpr(expr),
+                                        ..
+                                    }) => Box::new(Expr::JSXEmpty(expr.clone())),
+                                    JSXAttrValue::JSXElement(element) => {
+                                        Box::new(Expr::JSXElement(element.clone()))
+                                    }
+                                    JSXAttrValue::JSXFragment(fragment) => {
+                                        Box::new(Expr::JSXFragment(fragment.clone()))
+                                    }
+                                })
+                                .unwrap_or_else(|| {
+                                    Box::new(Expr::Lit(Lit::Bool(Bool {
+                                        span: DUMMY_SP,
+                                        value: true,
+                                    })))
+                                }),
+                        })))
+                    }
+                    JSXAttrOrSpread::SpreadElement(spread) => PropOrSpread::Spread(spread.clone()),
+                })
+                .collect(),
+        }
     }
 }
 
@@ -75,7 +119,7 @@ impl VisitMut for VueJsxTransformVisitor {
                         expr: Box::new(if jsx.opening.attrs.is_empty() {
                             Expr::Lit(Lit::Null(Null { span: DUMMY_SP }))
                         } else {
-                            Expr::Object(self.transform_props())
+                            Expr::Object(self.transform_attrs(&jsx.opening.attrs))
                         }),
                     },
                 ],
@@ -103,6 +147,6 @@ test!(
         imports: Default::default(),
     }),
     basic,
-    r#"const App = <Comp></Comp>;"#,
+    r#"const App = <Comp v={afa}></Comp>;"#,
     r#""#
 );
